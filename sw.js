bc8ad28d1996cb1237c16b2ae2857550fd64e47b
Ian Henckel <ian471@users.noreply.github.com>
2021-08-24 13:14:20 -0700

    Fix service-worker non-GET cache error

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).then(
       response => caches.open('preact-zero')
-        .then(cache => cache.put(e.request, response.clone()))
+        .then(cache => e.request.method === 'GET' && cache.put(e.request, response.clone()))
         .then(() => response),
       err => caches.match(e.request) || err
     )
